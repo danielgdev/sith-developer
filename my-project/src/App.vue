7676eb278174efc4198e808887bcbd4ac4b8331e
Daniel Galindo <hello@danielgalindo.dev>
2020-04-21 17:11:10 -0500

    vue

---
+++ my-project/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Proyecto en vue</h1>
+    <input v-model="name" />
+    <br />
+    <br />
+    <label>{{ toLower }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: ""
+    };
+  },
+  computed: {
+    toLower() {
+      return this.name.toLowerCase();
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
