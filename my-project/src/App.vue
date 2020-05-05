0a035fa7f65875b61987170714c792147ba16999
Daniel Galindo <hello@danielgalindo.dev>
2020-05-05 16:31:56 -0500

    render de lista

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,22 +1,26 @@
 <template>
-  <div id="app">
-    <h1 class="text-3xl text-gray-700 font-bold">Proyecto en vue</h1>
-    <br />
-    <br />
-    <label>Total de elementos: {{total_unidad}}</label>
-    <br />
-    <label>Total: {{total_precio}}</label>
-    <br />
-    <br />
-    <input v-model="name" />
-    <input v-model="price" />
-    <br />
-    <br />
-    <button @click="agregar">Agregar</button>
+  <div id="app" class="w-full min-h-screen flex">
+    <div class="w-1/2 min-h-screen p-4">
+      <h1 class="text-3xl text-gray-700 font-bold mb-8">Carro de compras en Vue</h1>
+      <input v-model="name" placeholder="Nombre del producto" />
+      <input v-model="price" />
+      <span v-if="'' != name && price > 0" class="inline-flex rounded-md shadow-sm mt-6">
+        <button
+          class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+          @click="agregar"
+        >Agregar</button>
+      </span>
+      <span v-else>
+        <p class="text-xs mt-4 text-red-300">Escribe el nombre de producto y un precio.</p>
+      </span>
+    </div>
+    <detalle :cart="cart" />
   </div>
 </template>
 
 <script>
+import Detalle from "./components/Detalle";
+
 export default {
   name: "App",
   data() {
@@ -34,31 +38,35 @@ export default {
       });
       this.name = "";
       this.price = 0;
-      console.log(this.cart);
     }
   },
-  computed: {
-    total_unidad() {
-      return this.cart.length;
-    },
-    total_precio() {
-      let total = 0;
-      for (const item of this.cart) {
-        total += parseFloat(item.price);
-      }
-      return total;
-    }
+  components: {
+    Detalle
   }
 };
 </script>
 
 <style>
-#app {
+html,
+body {
+  color: #4e4e4e;
+  font-size: 16px;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+}
+body {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 16px;
+}
+*,
+:after,
+:before {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ my-project/src/components/Detalle.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="detalle" class="w-1/2 min-h-screen h-full flex flex-col p-4">
+    <div class="w-full h-auto">
+      <p class="text-lg">
+        Total de elementos
+        <span class="font-bold">{{ total_unidad }}</span>
+      </p>
+    </div>
+    <div class="w-full flex-1 relative">
+      <div class="absolute inset-0 overflow-y-auto">
+        <ul class="w-full h-auto mt-4">
+          <li v-for="(item, index) in cart" :key="index" class="w-full h-auto flex justify-between">
+            <span class="text-lg font-bold text-gray-600">{{ item.name }}</span>
+            <span class="text-md">$ {{item.price}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="w-full h-auto">
+      <p class="text-3xl font-bold">Total $ {{total_precio}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["cart"],
+  name: "detalle",
+  computed: {
+    total_unidad() {
+      return this.cart.length;
+    },
+    total_precio() {
+      let total = 0;
+      for (const item of this.cart) {
+        total += parseFloat(item.price);
+      }
+      return total;
+    }
+  }
+};
+</script>
